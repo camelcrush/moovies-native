191f81a31f179dd27089fb0d671444d7fd40df23
camelcrush <dkxm0927@naver.com>
2021-10-27 21:34:53 +0900

    AppLoading Part 2: Font, Asset, Image

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Text } from "react-native";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Image, Text } from "react-native";
 
 export default function App() {
   const [ready, setReady] = useState(false);
-  const startLoading = async () => {};
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./loading.jpeg"));
+    await Image.prefetch("https://unsplash.com/photos/6tYNib97-8o");
+  };
   const onFinish = () => setReady(true);
   if (!ready) {
     return (
