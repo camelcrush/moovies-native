51b5b871790a542d02acaa7bb8dd0ea4f78c4781
camelcrush <dkxm0927@naver.com>
2021-10-27 21:58:09 +0900

    AppLoading Part 4: useAssets, useFonts

--- App.js
+++ App.js
@@ -1,40 +1,15 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Image, Text } from "react-native";
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require("./loading.jpeg"),
-      "https://unsplash.com/photos/6tYNib97-8o",
-    ]);
-    await Promise.all([...fonts, ...images]);
-  };
-  const onFinish = () => setReady(true);
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [assets] = useAssets([require("./loading.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done loading!</Text>;
 }
