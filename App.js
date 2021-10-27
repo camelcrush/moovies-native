1559935108ed187bc770d673d337c1342df6a747
camelcrush <dkxm0927@naver.com>
2021-10-27 22:03:07 +0900

    Fix: AppLoading

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Asset, useAssets } from "expo-asset";
+import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Image, Text } from "react-native";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
-  const [assets] = useAssets([require("./loading.jpeg")]);
-  const [loaded] = Font.useFonts(Ionicons.font);
-  if (!assets || !loaded) {
-    return <AppLoading />;
+  const [ready, setReady] = useState(false);
+  const startLoading = async () => {
+    const fonts = loadFonts([Ionicons.font]);
+    await Promise.all([...fonts]);
+  };
+  const onFinish = () => setReady(true);
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
   }
   return <Text>We are done loading!</Text>;
 }
