0bd2637635fb80de014a6939723ab9823ccaf2e3
camelcrush <dkxm0927@naver.com>
2021-10-27 21:53:26 +0900

    AppLoading Part 3: function & array

--- App.js
+++ App.js
@@ -5,12 +5,26 @@ import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Image, Text } from "react-native";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./loading.jpeg"));
-    await Image.prefetch("https://unsplash.com/photos/6tYNib97-8o");
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./loading.jpeg"),
+      "https://unsplash.com/photos/6tYNib97-8o",
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
   const onFinish = () => setReady(true);
   if (!ready) {
