af11b946463987a73787809339e3c28334311770
camelcrush <dkxm0927@naver.com>
2021-10-31 21:08:03 +0900

    TV Screen Part 3: Refreshing

--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,30 +1,43 @@
 import React from "react";
-import { FlatList, ScrollView } from "react-native";
-import { useQuery } from "react-query";
+import { FlatList, RefreshControl, ScrollView } from "react-native";
+import { useQuery, useQueryClient } from "react-query";
 import { tvApi } from "../api";
 import HList from "../components/HList";
 import Loader from "../components/Loader";
 import VMedia from "../components/VMedia";
 
 const Tv = () => {
-  const { isLoading: todayLoading, data: todayData } = useQuery(
-    ["tv", "today"],
-    tvApi.airingToday
-  );
-  const { isLoading: topLoading, data: topData } = useQuery(
-    ["tv", "top"],
-    tvApi.topRated
-  );
-  const { isLoading: trendingLoading, data: trendingData } = useQuery(
-    ["tv", "trending"],
-    tvApi.trending
-  );
+  const queryClient = useQueryClient();
+  const {
+    isLoading: todayLoading,
+    data: todayData,
+    isRefetching: todayRefetching,
+  } = useQuery(["tv", "today"], tvApi.airingToday);
+  const {
+    isLoading: topLoading,
+    data: topData,
+    isRefetching: topRefetching,
+  } = useQuery(["tv", "top"], tvApi.topRated);
+  const {
+    isLoading: trendingLoading,
+    data: trendingData,
+    isRefetching: trendingRefetching,
+  } = useQuery(["tv", "trending"], tvApi.trending);
+  const onRefresh = () => {
+    queryClient.refetchQueries(["tv"]);
+  };
   const loading = todayLoading || topLoading || trendingLoading;
+  const refreshing = todayRefetching || topRefetching || trendingRefetching;
   if (loading) {
     return <Loader />;
   }
   return (
-    <ScrollView contentContainerStyle={{ paddingVertical: 30 }}>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+      contentContainerStyle={{ paddingVertical: 30 }}
+    >
       <HList title="Trending TV" data={trendingData.results} />
       <HList title="Airing Today" data={todayData.results} />
       <HList title="Top Rated TV" data={topData.results} />
