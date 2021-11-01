6e4123465a6d13aeedac8202795a78b06272fb97
camelcrush <dkxm0927@naver.com>
2021-11-01 19:49:31 +0900

    Searc Part 1

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components/native";
 import { Text, View } from "react-native";
 
-const Search = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Search</Text>
-  </View>
-);
+const Container = styled.ScrollView``;
+
+const SearchBar = styled.TextInput`
+  background-color: white;
+  width: 90%;
+  border-radius: 15px;
+  padding: 10px 15px;
+  margin: 10px auto;
+`;
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const onChangetext = (text: string) => setQuery(text);
+  return (
+    <Container>
+      <SearchBar
+        placeholder="Search for Movie or TV Show"
+        placeholderTextColor="gray"
+        onChangeText={onChangetext}
+        returnKeyType="search"
+      />
+    </Container>
+  );
+};
 
 export default Search;
